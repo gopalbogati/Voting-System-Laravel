2dbb9ea1d1ec39072538d51191f2b0a6f2e1ab1b
Gopal Bogati <gopal2736@gmail.com>
2021-04-28 14:19:31 +1000

    Search bar indentation fix

--- voting-system/resources/views/index.blade.php
+++ voting-system/resources/views/index.blade.php
@@ -21,12 +21,31 @@
 
 		<div class="w-1/3 relative">
 
-			<input type="search" placeholder="find an idea" class="w-full rounded-xl border-none bg-white px-4 py-2 pl-8 placeholder-gray-900">
+		   <input type="search" placeholder="find an idea" class="w-full rounded-xl border-none bg-white px-4 py-2 pl-8 placeholder-gray-900">
+			<div class="absolute top-0 flex items-center h-full ml-2">
+				<svg class="w-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+				</svg>
+			</div>
 		</div> 
-		<div class="absolute top-0 flex items-center h-full ml-2">
-			<svg class="w-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-			</svg>
-		</div>
+		
+		{{-- 	End filter --}}
+		{{-- <div class="ideas-container space-y-6 my-6">
+			<div class="ideas-container bg-white rounded-xl flex">
+				<div class="border-r border-gray-100 px-5 py-8">
+					<div class="text-center">
+						<div class="font-semifold text-2xl">12</div>
+						<div class="text-gray-500">Votes</div>
+
+						<div class="mt-8">
+							<button class="w-20 border-gray-200 font-bold text-xs uppercase rounded-xl px-4 py-3">Vote	
+							</button>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+		</div> --}}
 
 	</x-app-layout>
