1d4ec2fbaaa496bde19004680c328803faeafd67
Gopal Bogati <gopal2736@gmail.com>
2021-04-28 11:57:22 +1000

    Added & divided navigation menu in 5 sections

--- voting-system/resources/views/layouts/app.blade.php
+++ voting-system/resources/views/layouts/app.blade.php
@@ -58,13 +58,32 @@ obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
 nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
 tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
 quia. 
-            </div>
+</div>
 <div class="w-175">
     <nav class="flex item-center justify-between text-xs">
-        <ul class="flex uppercase font-semibold space-x-10">
-            <li><a href="#" class="">All Ideas(88)</li>
+        <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+            <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">All Ideas(88)</li>
+            <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering(6)</li> 
+            <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">In Progress(1)</li> 
+         </ul>
 
+
+         <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+            <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Implemented(6)</li>
+           
+            <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed(55)</li> 
+           
+         </ul>
+
+      {{-- <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+
+            <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Implemented(10)</li> 
+            <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed(55)</li> 
+       
         </ul>
+          --}}
+
+        
 
     </nav>
 </div>        
